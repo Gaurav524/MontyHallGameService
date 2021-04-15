b71588ed5fee3f8c77206fa7b7050350a3702b2f
Gaurav Gupta <GGUPTA@volvocars.com>
2021-04-15 12:56:21 +0200

    Added test project

---
+++ test/GameService.Api.UnitTest/AutoMapperProfileTest.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Xunit;
+
+namespace GameService.Api.UnitTest
+{
+    public  class AutoMapperProfileTest
+    {
+        [Fact]
+        public void AutoMapper_CorrectConfiguration_WillNotThrowException()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<Converters.AutoMapperProfile>();
+            });
+
+            var mapper = config.CreateMapper();
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
---
+++ test/GameService.Api.UnitTest/GameRequestHandlerTest.cs
@@ -0,0 +1,80 @@
+﻿using GameService.Api.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using GameService.Api.Contracts;
+using GameService.Core.Interfaces;
+using GameService.Core.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace GameService.Api.UnitTest
+{
+    public class GameRequestHandlerTest
+    {
+        private GameRequestHandler _sut;
+        private readonly Mock<IMapper> mapperMock;
+        private readonly Mock<IEmulateGameService> emulateGameServiceMock;
+        private readonly Mock<ILogger<GameRequestHandler>> loggerMock;
+
+        public GameRequestHandlerTest()
+        {
+            emulateGameServiceMock = new Mock<IEmulateGameService>();
+            mapperMock = new Mock<IMapper>();
+            loggerMock = new Mock<ILogger<GameRequestHandler>>();
+
+            _sut = new GameRequestHandler(
+                emulateGameServiceMock.Object,
+                mapperMock.Object,
+                loggerMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_GameRequestWithZeroSimulations_WillReturnNull()
+        {
+            var gameRequest = new GameRequest
+            {
+                NumberOfSimulations = 0,
+                ReadyToChangeDoor = true
+            };
+
+            var response = await _sut.Handle(gameRequest, default);
+
+            response
+                .Should()
+                .BeNull();
+        }
+
+        [Fact]
+        public async Task Handle_GameRequestWithZeroSimulations_WillNotReturnNull()
+        {
+            var gameRequestSpecification = new GameRequestSpecification()
+            {
+                NumberOfSimulations = 100,
+                ReadyToChangeDoor = true
+            };
+
+            mapperMock.Setup(x =>
+                    x.Map<GameRequestSpecification>(It.IsAny<GameRequest>()))
+                .Returns(gameRequestSpecification);
+
+
+            var gameRequest = new GameRequest
+            {
+                NumberOfSimulations = 100,
+                ReadyToChangeDoor = true
+            };
+
+            var response = await _sut.Handle(gameRequest, default);
+
+            response
+                .Should()
+                .NotBeNull();
+        }
+    }
+}
